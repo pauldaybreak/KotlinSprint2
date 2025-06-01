46e2c0dbce030d2b6d6d206be2ffa4dfb02a02e4
pmochalov <paulmochaloff@gmail.com>
2025-06-01 23:43:20 +0300

    KS-1-1-task1

---
+++ src/main/kotlin/lesson_1/Task1.kt
@@ -0,0 +1,7 @@
+package org.example.lesson_1
+
+fun main() {
+    val quantityOrders: Int = 5
+    val textOfThnx: String = "Благодарим за покупку, если приведёте друга, дадим скидку 10%"
+    println("Информация о заказах: $quantityOrders, текст для клиента: $textOfThnx")
+}
