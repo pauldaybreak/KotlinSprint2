08835866faba92375e79e26570855c8954a5738b
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-02 14:07:35 +0300

    KS-1-3 task3 delete println fly to space

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,12 @@
 package org.example
 
-fun main() {
-    println("Hello World!")
+
+fun main(){
+    val year: Int = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+    print("$year, $hour:$minute")
+
+
+
 }
