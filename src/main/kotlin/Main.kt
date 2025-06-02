1845f9b0f2f00f74f69b9b3de889dc3f13b5c2af
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-02 16:15:16 +0300

    KS1-1-5 task 5

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package org.example
 
 fun main() {
-    println("Hello World!")
+
+        var totalSeconds = 6430L
+
+        val hours = totalSeconds / 3600
+        val ostatokSec = totalSeconds % 3600
+        val minutes = ostatokSec / 60
+        val seconds = ostatokSec % 60
+
+    println(ostatokSec)
+    println("%02d:%02d:%02d".format(hours, minutes, seconds))
 }
