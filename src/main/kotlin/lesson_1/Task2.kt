ac910807fd11779cc97fd1bc1b1e036e64d8065f
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-02 13:51:01 +0300

    KS-1-2-task2 println of new value quantityWorkers

--- src/main/kotlin/lesson_1/Task2.kt
+++ src/main/kotlin/lesson_1/Task2.kt
@@ -4,7 +4,10 @@ fun main() {
     val quantityOrders: Int = 5
     val textOfThnx: String = "Благодарим за покупку, если приведёте друга, дадим скидку 10%"
     println("Информация о заказах: $quantityOrders, текст для клиента: $textOfThnx")
-    val quantityWorkers: Int = 10
+    var quantityWorkers: Int = 10
+    //println("Количество работников интернет-магазина $quantityWorkers")
+    quantityWorkers = 9
     println("Количество работников интернет-магазина $quantityWorkers")
+
 
 }
