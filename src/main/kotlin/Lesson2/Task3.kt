2c008087be580bba7d5bace8b6aa5f4c38fee489
pmochalov <paulmochaloff@gmail.com>
2025-06-02 22:37:11 +0300

    KS2-3 Task3

---
+++ src/main/kotlin/Lesson2/Task3.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson2
+
+fun main() {
+    val hourTo = 9
+    val minutes = 40
+    val timeRoad = 457
+
+    val minutesOfDay = (60 * hourTo) + minutes
+    val timeMinutesFinish = (minutesOfDay + timeRoad) % 60
+    val timeHourFinish = (minutesOfDay + timeRoad) / 60
+
+    println("%02d:%02d".format(timeHourFinish, timeMinutesFinish))
+}
