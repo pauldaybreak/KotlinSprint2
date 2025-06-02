c3c52265e322119847f0f80e0ec8bd93e48ac54a
pmochalov <paulmochaloff@gmail.com>
2025-06-02 22:49:58 +0300

    KS2-4 Task4

---
+++ src/main/kotlin/Lesson2/Task4.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson2
+
+fun main(){
+    var crystalWeight = 7
+    var ferrumWeght = 11
+    val buff = 20
+
+    crystalWeight = (crystalWeight * buff) / 100
+    ferrumWeght = (ferrumWeght * buff) / 100
+
+    println(crystalWeight)
+    println(ferrumWeght)
+}
