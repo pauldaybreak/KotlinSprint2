a6ac0a2f4eadf461326c9068aa321ec16852b9af
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-03 15:00:17 +0300

    KS-2-2-task2

---
+++ src/main/kotlin/lesson_2/Task2.kt
@@ -0,0 +1,14 @@
+package lesson_2
+
+fun main(){
+    val quantityWorkers : Int = 50
+    val salaryWorker : Int = 30000
+    val salaryIntern : Int = 20000
+    val quantityIntern : Int = 30
+
+    val costWorkers = quantityWorkers * salaryWorker
+    val costWorkersAndIntern = costWorkers + (salaryIntern * quantityIntern)
+    val middleCost =  costWorkersAndIntern/(quantityWorkers + quantityIntern)
+
+    println("costWorkers $costWorkers, costWorkersAndIntern $costWorkersAndIntern, middleCost $middleCost")
+}
