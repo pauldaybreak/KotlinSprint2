fa1c5b581806053bc7a3540252c31fa52f6ba712
pmochalov <paulmochaloff@gmail.com>
2025-06-04 08:55:26 +0300

    KS-1-5-task5 1) Вывел 60 в константу. 2) 3600 тоже в константу, хотя кажется избыточным.

--- src/main/kotlin/lesson_1/Task5.kt
+++ src/main/kotlin/lesson_1/Task5.kt
@@ -1,13 +1,15 @@
 package lesson_1
 
 fun main() {
+    val CONST_HOUR = 60
+    val CONST_HOUR_IN_SEC = CONST_HOUR*6
 
     var totalSeconds = 6430L
 
-    val hours = totalSeconds / 3600
-    val remainigSec = totalSeconds % 3600
-    val minutes = remainigSec / 60
-    val seconds = remainigSec % 60
+    val hours = totalSeconds / (CONST_HOUR_IN_SEC)
+    val remainigSec = totalSeconds % CONST_HOUR_IN_SEC
+    val minutes = remainigSec / CONST_HOUR
+    val seconds = remainigSec % CONST_HOUR
 
     println(remainigSec)
     println("%02d:%02d:%02d".format(hours, minutes, seconds))
