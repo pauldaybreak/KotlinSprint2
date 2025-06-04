8d84b678222eeb8ad099cdbda8482f0ef286b8a2
pmochalov <paulmochaloff@gmail.com>
2025-06-04 08:31:46 +0300

    KS-1-3-task3 1)Изменил значения на время посадки. 2) Вывел в одной строке.

--- src/main/kotlin/lesson_1/Task3.kt
+++ src/main/kotlin/lesson_1/Task3.kt
@@ -4,8 +4,9 @@ fun main(){
     val year: String = "1961"
     var hour: String = "09"
     var minute: String = "07"
-    println(year)
-    println(hour)
-    println(minute)
+
+    hour = "10"
+    minute = "55"
+    println("$hour:$minute")
 
 }
