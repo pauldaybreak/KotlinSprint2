ffbcd808f869e0ac4c4ed96f2d60720ae7ec3358
pmochalov <paulmochaloff@gmail.com>
2025-06-04 08:27:47 +0300

    KS-1-3-task3 1)Объявил переменные строкой 2)Вывел переменные взлёта на каждой строке.

--- src/main/kotlin/lesson_1/Task3.kt
+++ src/main/kotlin/lesson_1/Task3.kt
@@ -1,9 +1,11 @@
 package lesson_1
 
 fun main(){
-    val year: Int = 1961
-    var hour: Int = 10
-    var minute: Int = 55
-    print("$hour:$minute")
+    val year: String = "1961"
+    var hour: String = "09"
+    var minute: String = "07"
+    println(year)
+    println(hour)
+    println(minute)
 
 }
