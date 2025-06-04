136a6c9ebf30399c1cd29e19e886f612c75f7254
pmochalov <paulmochaloff@gmail.com>
2025-06-04 22:05:26 +0300

    KS-2-4-task4 Подправил название переменной, добавил комментарии, изменил вывод

--- src/main/kotlin/lesson_2/Task4.kt
+++ src/main/kotlin/lesson_2/Task4.kt
@@ -1,14 +1,17 @@
 package lesson_2
 
+/*
+Расчёт ресурсов данные баффом
+ */
 
 fun main(){
     var crystalWeight = 7
-    var ferrumWeght = 11
+    var ferrumWeight = 11
     val buff = 20
 
     crystalWeight = (crystalWeight * buff) / 100
-    ferrumWeght = (ferrumWeght * buff) / 100
+    ferrumWeight = (ferrumWeight * buff) / 100
 
-    println(crystalWeight)
-    println(ferrumWeght)
+    println("Кристаллы данные баффом: " + crystalWeight)
+    println("Железная руда данная баффом: " + ferrumWeight)
 }
