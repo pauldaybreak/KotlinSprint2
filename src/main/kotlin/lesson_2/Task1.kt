04db0db6155cd20ec52c4dbee6279b07d069b15a
pmochalov <paulmochaloff@gmail.com>
2025-06-05 21:09:58 +0300

    KS-2-1-task1  Преобразовал сумму оценок в Double

--- src/main/kotlin/lesson_2/Task1.kt
+++ src/main/kotlin/lesson_2/Task1.kt
@@ -9,9 +9,9 @@ fun main(){
     val resultStudent2 = 4
     val resultStudent3 = 3
     val resultStudent4 = 5
-    val resultToDouble = 1.0 // что б средняя оценка была Double
+
     val countOfStudents = 4 // количетсво студентов
-    val averageResultOfStudents: Double = (resultStudent1 + resultStudent2 + resultStudent3 + resultStudent4) * resultToDouble / countOfStudents
+    val averageResultOfStudents: Double = (resultStudent1 + resultStudent2 + resultStudent3 + resultStudent4).toDouble() / countOfStudents
 
     print(averageResultOfStudents)
 }
