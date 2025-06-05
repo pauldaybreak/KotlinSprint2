fcf4a05645fa77d86de0256ad0cbc8d298374a7c
pmochalov <paulmochaloff@gmail.com>
2025-06-05 21:31:41 +0300

    KS-2-5-task5  Объявил переменную resultInvest сразу со значением. Вместо цикла - функция pow(). Автоформат согласно стилистике кода.

--- src/main/kotlin/lesson_2/Task5.kt
+++ src/main/kotlin/lesson_2/Task5.kt
@@ -3,18 +3,14 @@ package lesson_2
 import kotlin.math.pow
 import kotlin.time.times
 
-fun main(){
-    val resultInvest: Double // результат вклада
+fun main() {
+
     val startSum: Double = 700000.000
     val percentTax: Double = 16.7
     val countOfPeriod = 20
-    var rate: Double = 1.0 // степень
-    val percent: Double = percentTax/100 // привести проценты для матем. операций
+    val percent: Double = percentTax / 100 // привести проценты для матем. операций
 
-    for(i in 1..countOfPeriod) {
-        rate *= (1 + percent)
-    }
-    resultInvest = (startSum*rate)/10
+    val resultInvest = startSum * (1 + percent).pow(countOfPeriod) / 10 // результат вклада
     println(String.format("%.3f", resultInvest))
 }
 
