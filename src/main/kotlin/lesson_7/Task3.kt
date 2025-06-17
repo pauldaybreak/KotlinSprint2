e3fb8b9736ee05beab903252cc8e5290f7253b9c
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-17 08:42:41 +0300

    KS-7-3-task3 Убрал преобразование toDouble.

--- src/main/kotlin/lesson_7/Task3.kt
+++ src/main/kotlin/lesson_7/Task3.kt
@@ -1,17 +1,14 @@
 package lesson_7
 
-
 import kotlin.text.StringBuilder
 
-// Вывод чётных чисел в консоль
-
 fun main() {
-    val userInputNumber = readln().toInt() // Пользовательский ввод
-    val progressiveNum = StringBuilder() // Полученное число
-    for (i in 0..userInputNumber step 2) { // Цикл с прогрессией
+    println("Введите число: ")
+    val userInputNumber = readln().toInt()
+    val progressiveNum = StringBuilder()
+    for (i in 0..userInputNumber step 2) {
         progressiveNum.append(i)
-
     }
-    progressiveNum.toString().toDouble() // Преобразую в Double, т.к. число и может быть очень большим
+    progressiveNum.toString()
     print(progressiveNum)
 }
