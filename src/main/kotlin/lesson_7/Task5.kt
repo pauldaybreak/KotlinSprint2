c359e1c6ffcc391044a6fcf302a4c32a20d4658f
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-17 08:58:52 +0300

    KS-7-5-task5 убрал комменты, сложил символы, вместо перечисления.

--- src/main/kotlin/lesson_7/Task5.kt
+++ src/main/kotlin/lesson_7/Task5.kt
@@ -1,13 +1,11 @@
 package lesson_7
 
-// Генерация пароля
-
-const val MIN_LENGTH_PASS = 6 // Минимальная длина пароля
+const val MIN_LENGTH_PASS = 6
 
 fun main() {
     do {
-        println("Введите число, которое будет длинной для сгенерированного пароля. Число-длина пароля, должна быть не менее 6")
-        val lengthOfPass = readln().toInt() // пользовательский ввод длины пароля
+        println("Введите число, (не меньше 6) для генерации пароля)")
+        val lengthOfPass = readln().toInt()
         if (lengthOfPass < MIN_LENGTH_PASS)
             println("Вы ввели длину меньше 6 символов")
         else if (lengthOfPass >= MIN_LENGTH_PASS) {
@@ -16,10 +14,8 @@ fun main() {
     } while (lengthOfPass < MIN_LENGTH_PASS)
 }
 
-// ф-ция генерации пароля с длиной от пользователя
-
 fun generationPassword(lengthPass: Int): String {
-    val rangeSymbol = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz1234567890"
+    val rangeSymbol =  ('a'..'z') + ('A'..'Z') + ('0'..'9')
     val generationPass = StringBuilder()
     for (i in 0 until lengthPass) {
         generationPass.append(rangeSymbol.random())
