dd21c7f72f990f91ded43a73a1502fde0a502b88
pmochalov <paulmochaloff@gmail.com>
2025-06-18 22:49:27 +0300

    KS-10-1-task1 Игра с машиной в кубик

--- src/main/kotlin/lesson_10/Task1.kt
+++ src/main/kotlin/lesson_10/Task1.kt
@@ -3,30 +3,40 @@ package lesson_10
 import kotlin.collections.mutableListOf
 import kotlin.random.Random
 
-fun main(){
-    var score = mutableListOf<Pair<Int, String>>()
+const val SIZE_KUBIK = 6
+
+fun main() {
+    val score = mutableListOf<Pair<Int, String>>()
+    println("Введите количество бросков")
     val countGame: Int = readln().toInt()
-    for(i in 0 until countGame){
+    for (i in 0 until countGame) {
         val humanGame = kickOfGameBox()
         println("бросает человек $humanGame")
         score.add(humanGame to "человек")
         val terminatorGame = kickOfGameBox()
         println("бросает машина $terminatorGame")
-        score.add(terminatorGame to "бросает машина")
+        score.add(terminatorGame to "машина")
 
     }
     sumOfScores(score)
 
 }
 
-fun kickOfGameBox(): Int{
-    val count: Int = Random.nextInt(1,6)
-    return count
+fun kickOfGameBox(): Int = Random.nextInt(1, SIZE_KUBIK)
+
+fun sumOfScores(score: MutableList<Pair<Int, String>>) {
+    var sumScoreHuman: Int = 0
+    var sumScoreTerminator: Int = 0
+    for ((value, player) in score) {
+        if (player == "человек") {
+            sumScoreHuman += value
+
+        } else if (player == "машина") {
+            sumScoreTerminator += value
+        }
+    }
+    println("Человек $sumScoreHuman, Машина $sumScoreTerminator")
+    if (sumScoreHuman > sumScoreTerminator) println("Человек победил $sumScoreHuman")
+    else println("Машина победила $sumScoreTerminator")
 }
 
-fun sumOfScores(score: MutableList<Pair<Int, String>>){
-    for(i in score){
-        if(values == "")
-        print(" $i")
-    }
-}
