4b9a9561b8a5863b93b7ee6f5388b507f108bc60
pmochalov <paulmochaloff@gmail.com>
2025-06-19 20:55:45 +0300

    KS-10-3 Генерация пароля

---
+++ src/main/kotlin/lesson_10/Task3.kt
@@ -0,0 +1,16 @@
+package lesson_10
+
+fun main() {
+    val lengthPass: Int = readln().toInt() / 2
+    println("пароль:\n${generationPass(lengthPass)}")
+}
+
+fun generationPass(length: Int): String {
+    val specialsymbols = " !\"#\$%&'()*+,-./"
+    val numSymbols = 0..9
+    val pass = StringBuilder()
+    repeat(length) {
+        pass.append(numSymbols.random(), specialsymbols.random())
+    }
+    return pass.toString()
+}
