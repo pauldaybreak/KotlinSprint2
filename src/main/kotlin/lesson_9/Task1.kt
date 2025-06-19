a15a69395afb1d2248503df6478a266c10110101
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-19 09:55:00 +0300

    KS-9-1-task1 Удалил лишние циклы

--- src/main/kotlin/lesson_9/Task1.kt
+++ src/main/kotlin/lesson_9/Task1.kt
@@ -1,6 +1,6 @@
 package lesson_9
 
-// Список ингридиентов
+
 
 fun main(){
     var listOfIngridients: MutableList<String> = listOf("морковь", "грибы", "картошка", "свекла", "соль", "перец", "курица") as MutableList<String>
