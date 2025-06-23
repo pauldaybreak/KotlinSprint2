13aff253be1e3d81161885ea5a29fd63bb08978c
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-23 14:04:37 +0300

    KS-10-3 specialSymbols сделал диапозоном ' '..'/', поменял цикл на for

--- src/main/kotlin/lesson_10/Task3.kt
+++ src/main/kotlin/lesson_10/Task3.kt
@@ -1,18 +1,19 @@
 package lesson_10
 
 fun main() {
+    println("Введите пожалуйста длину пароля: ")
     val lengthPass: Int = readln().toInt()
     println("пароль:\n${generatePassword(lengthPass)}")
 }
 
 fun generatePassword(length: Int): String {
-    val specialSymbols = 32..47
+    val specialSymbols = ' '..'/'
     val numberSymbols = 0..9
     val password = StringBuilder()
 
-    (0 until length).forEach { i ->
+    for (i in 0 until length){
         if (i % 2 == 0) password.append(numberSymbols.random())
-        else password.append(specialSymbols.random().toChar())
+        else password.append(specialSymbols.random())
     }
 
     return password.toString()
