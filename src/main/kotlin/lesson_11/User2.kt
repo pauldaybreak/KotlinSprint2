7629836d97e3e5a7db81100bbb5ed517c38283b2
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-24 13:30:47 +0300

    KS-11-2 подредактировал написание кода.

--- src/main/kotlin/lesson_11/User2.kt
+++ src/main/kotlin/lesson_11/User2.kt
@@ -1,24 +1,27 @@
 package lesson_11
 
-class User2 (
+class User2(
     val userId: Int,
     val login: String,
     var password: String,
     val email: String,
     var bio: String = "я студнет KotlinSprint"
 ) {
-    fun printUserInformation(){
+
+    fun printUserInformation() {
         println("$userId, $login, $password, $email, $bio")
     }
-    fun inputUserBioInformation(){
+
+    fun inputUserBioInformation() {
         println("Введите свою краткую биографию:")
         val inputUser = readln()
         bio = inputUser
     }
-    fun changePassword(){
+
+    fun changePassword() {
         println("Введите действующий пароль:")
         var inputPassword = readln()
-        if(inputPassword == password){
+        if (inputPassword == password) {
             println("Введите новый пароль:")
             var newPassword = readln()
             password = newPassword
@@ -39,22 +42,15 @@ fun main() {
     user1.changePassword()
     user1.printUserInformation()
 
-
     val user2: User2 = User2(
         userId = 2,
         login = "camel2023",
         password = "lflghkhkp",
         email = "camel@post.com",
-
-        )
+    )
     user2.printUserInformation()
     user2.inputUserBioInformation()
     user2.changePassword()
     user2.printUserInformation()
-
-
-    //   println("${user1.userId}, ${user1.login}, ${user1.password}, ${user1.email}, ${user1.bio}")
-    //   println("${user2.userId}, ${user2.login}, ${user2.password}, ${user2.email}, ${user2.bio}")
-
 }
 
