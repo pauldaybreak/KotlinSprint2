eb89f0ecf806ac17f8218ba25d4a3318bd146993
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-06-27 15:50:05 +0300

    KS-11-4 переместил listOfIngridient в Recipe

--- src/main/kotlin/RecipeApp.kt
+++ src/main/kotlin/RecipeApp.kt
@@ -1,44 +1,40 @@
 class RecipeApp {}
 
-    class CategoryRecipe(
-        var idCategory: Int,
-        var nameCategory: String,
-        var listOfRecipe: MutableList<Recipe>
+class CategoryRecipe(
+    var idCategory: Int,
+    var nameCategory: String,
+    var listOfRecipe: MutableList<Recipe>
 
-        ) {}
+) {}
 
-    class Recipe(
-        val idRecipe: Int,
-        val nameRecipe: String,
-        var flagFavouriteRecipe: Boolean = false,
-        var descriptionRecipe: String
+class Recipe(
+    val idRecipe: Int,
+    val nameRecipe: String,
+    var flagFavouriteRecipe: Boolean = false,
+    var descriptionRecipe: String,
+    var listOfIngridient: MutableList<Triple<String, Int, String>>
 
-        ) {}
+) {}
 
-    class Ingridient(
-        val ingridientsForRecipe : Recipe,
-        var listOfIngridient: MutableList<Triple<String, Int ,String>>
-        ) {}
-
-
-    fun main() {
-        val recipe1 = Recipe(1, "Классический бургер", true, "берем ингридиенты, готовим бургер")
-        val recipe2 = Recipe(2, "Чизбургер", false, "берем ингридиенты, готовим чизбургер")
-        var category1 = CategoryRecipe(
-            1, "Бургеры",
-            listOfRecipe = mutableListOf(recipe1, recipe2))
-
-        var listIngridientsForRecipe1 = Ingridient(
-            ingridientsForRecipe = recipe1,
-            listOfIngridient = mutableListOf(
+fun main() {
+    val recipe1 = Recipe(
+        1, "Классический бургер", true,
+        listOfIngridient = mutableListOf(
             Triple("Булка", 1, "штука"),
             Triple("Котлета", 1, "штука"),
             Triple("Сыр", 40, "грамм")
-        )
-        )
-
-
-
-    }
+        ),
+        descriptionRecipe = "берем ингридиенты, готовим бургер"
+    )
+    val recipe2 = Recipe(
+        1, "Чизбургер", true,
+        listOfIngridient = mutableListOf(
+            Triple("Булка", 1, "штука"),
+            Triple("Курица", 1, "штука"),
+            Triple("Петрушка", 40, "грамм")
+        ),
+        descriptionRecipe = "берем ингридиенты, готовим чизбургер"
+    )
+}
 
 
