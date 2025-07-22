44f5fc1accc2e712135a5a72b533745eda6cb96b
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-07-22 10:00:18 +0300

    KS-13-3 изменил переменные на val; убрал генерацию компаний; убрал цикл и просто вывел в println

--- src/main/kotlin/lesson_13/PhoneBookCompany.kt
+++ src/main/kotlin/lesson_13/PhoneBookCompany.kt
@@ -3,48 +3,25 @@ package lesson_13
 import kotlin.random.Random
 
 class PhoneBookCompany(
-    var name: String,
-    var phoneNumber: Long,
-    var company: String? = null,
-) {
-    fun printInfo() {
-        println("- Имя: $name\n- Номер: $phoneNumber\n- Компания: $company")
-        println("---")
-    }
-
-}
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {}
 
 fun generateNumber(): Long {
     return Random.nextLong(10000000, 900000000)
 }
 
-fun generateChoiseCompany(): String {
-    val listOfCompany = listOf<String>(
-        "Яндекс",
-        "Сбербанк",
-        "X5 Group",
-        "Тинькофф",
-        "Ozon",
-        "Wildberries",
-        "null",
-    )
-    return listOfCompany.random().toString()
-}
-
 fun main() {
-    var listWithContact = mutableListOf(
+    val listWithContact = mutableListOf(
         PhoneBookCompany("Андрей", generateNumber(), "null"),
         PhoneBookCompany("Борис", generateNumber(), null),
         PhoneBookCompany("Владимир", generateNumber(), null),
-        PhoneBookCompany("Генадий", generateNumber(), generateChoiseCompany()),
-        PhoneBookCompany("Дмитрий", generateNumber(), generateChoiseCompany()),
+        PhoneBookCompany("Генадий", generateNumber(), "Тинькофф"),
+        PhoneBookCompany("Дмитрий", generateNumber(), "ЧелябМеталлИнвест"),
     )
-    for (i in listWithContact) {
-        i.printInfo()
-    }
+
 
     val listCompaniesOfPhoneBook = listWithContact.mapNotNull { it.company }.toSet()
-    for (i in listCompaniesOfPhoneBook) {
-        println(i)
-    }
+    println("Список компаний: ${listCompaniesOfPhoneBook.joinToString(", ")}")
 }
