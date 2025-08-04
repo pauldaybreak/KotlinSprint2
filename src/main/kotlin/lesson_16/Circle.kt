3793de86204e855e3e600387cf247dfdcdf8f04c
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-08-04 09:14:17 +0300

    KS-16-2 лишние пробелы убрал.

--- src/main/kotlin/lesson_16/Circle.kt
+++ src/main/kotlin/lesson_16/Circle.kt
@@ -10,12 +10,10 @@ class Circle(
 
     fun lengthOfCircle(): Double {
         return 2 * PI * radius
-
     }
 
     fun squareCircle(): Double {
         return PI * radius.toDouble().pow(2.0)
-
     }
 }
 
@@ -23,5 +21,4 @@ fun main() {
     val circle1 = Circle(3)
     println("Длина окружности -  ${circle1.lengthOfCircle()}")
     println("Площадь круга - ${circle1.squareCircle()}")
-
 }
