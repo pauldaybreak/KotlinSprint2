bd4072af95c1a16e3f4f2de110143e07438c143a
WstAdm.Mochalov <paulmochaloff@gmail.com>
2025-08-04 16:05:29 +0300

    KS-16-1 Использовал random(), убрал лишний импорт.

--- src/main/kotlin/lesson_16/GameCube.kt
+++ src/main/kotlin/lesson_16/GameCube.kt
@@ -1,16 +1,14 @@
 package lesson_16
 
-import kotlin.random.Random
+class GameCube {
+    private val numberOnSideCube = (1..6).random()
 
-class GameCube{
-    private val numberOnSideCube = Random.nextInt(1, 7)
-
-    fun printNumber(){
+    fun printNumber() {
         println(numberOnSideCube)
     }
 }
 
-fun main(){
+fun main() {
     val game1 = GameCube()
     game1.printNumber()
 }
